239a1bcb949d9edfef6bec708dd2fdbebd6fd3ab
misho <mihailirinchev@gmail.com>
2014-02-24 22:12:12 +0200

    Task 4

---
+++ Person/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Person
+{
+	internal class Person
+	{
+		public string Name { get; set; }
+
+		public uint? Age { get; set; }
+
+		public override string ToString()
+		{
+			return String.Format("Student(Name: {0}, Age: {1})",
+				string.IsNullOrEmpty(this.Name) ? "<unnamed>" : this.Name,
+				this.Age.HasValue ? this.Age.Value.ToString() : "<not specified>");
+		}
+	}
+}
---
+++ Person/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Person
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			var ivan = new Person();
+			ivan.Name = "Ivan";
+
+			Console.WriteLine(ivan);
+
+			var pesho = new Person();
+			pesho.Name = "Pesho";
+			pesho.Age = 7;
+
+			Console.WriteLine(pesho);
+		}
+	}
+}
